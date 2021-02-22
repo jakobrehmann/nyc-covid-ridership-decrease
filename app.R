671c9ec58fbac48ef0a894f55153054e19a98462
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-02-22 18:00:22 +0100

    final version

--- app.R
+++ app.R
@@ -2,7 +2,8 @@ library(shiny)
 library(tmap)
 library(viridis)
 library(tidyverse)
-library()
+library(sf)
+library(sfnetworks)
 setwd("C:\\Users\\jakob\\projects\\nyc-covid-ridership-decrease")
 
 monthStart <- function(x) {
@@ -40,12 +41,13 @@ load("net2.Rda")
 ###################### UI #########################
 
 ui <- fluidPage(
+  titlePanel("New York City Subway System - Data Analysis"),
   tabsetPanel(
     tabPanel("MTA Subway Ridership - COVID",
-  titlePanel("MTA Subway Ridership"),
   #splitLayout(
+  flowLayout(
     selectInput("demographic",
-                           "Demographic Data", 
+                           "Demographic Data:", 
                            c("Median Income", 
                              "Race: Percent White",
                              "Race: Percent Black",
@@ -54,13 +56,13 @@ ui <- fluidPage(
                              "Race: Percent 2orMore",
                              "Primary Work Transport: PT",
                              "Primary Work Transport: Car")),
-    sliderInput("date", "Time",
+    sliderInput("date", "Date:",
                           min = as.Date("2020-01-01"),
                           max =as.Date("2020-12-31"),
                           value=as.Date("2020-01-01"),
                           timeFormat="%b %Y", 
                           animate = animationOptions(interval = 100))              
-   # )
+    )
   ,
   
   splitLayout(tmapOutput(outputId = "map"),
@@ -68,91 +70,43 @@ ui <- fluidPage(
               )
   ),
   
-  tabPanel("Network Analysis",  splitLayout(tmapOutput(outputId = "cent1"),tmapOutput(outputId = "cent2")))
+  tabPanel("Network Analysis - Centrality",  splitLayout(tmapOutput(outputId = "cent1"),tmapOutput(outputId = "cent2")))
   )
 )
 
 server <- function(input, output, session) { 
 
   output$map <- renderTmap({
-  
-    if(input$demographic == "Median Income") {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "income_median", 
-                    breaks = c(0, 50000, 100000, 150000, 250000),
-                    palette = vir, 
-                    alpha = 0.6, 
-                    border.alpha = 0.5, 
-                    title = "Median Household Income in 2019, USD",
-                    id = "income_median") +
-        tm_layout(legend.position = c("right", "bottom"), title.position = c('right', 'bottom'))
-    } else if(input$demographic == "Race: Percent White")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "race_white", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Race: Pct White",
-                    id = "race_white")
-    }else if(input$demographic == "Race: Percent Black")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "race_black", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Race: Pct Black",
-                    id = "race_black")
-    }else if(input$demographic == "Race: Percent Asian")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "race_asian", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Race: Pct Asian",
-                    id = "race_asian")
-    }else if(input$demographic == "Race: Percent Other")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "race_other", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Race: Pct Other",
-                    id = "race_other")
-    }else if(input$demographic == "Race: Percent 2orMore")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "race_2ormore", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Race: Pct 2orMore",
-                    id = "race_2ormore")
-    }else if(input$demographic == "Primary Work Transport: PT")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "transport_pt", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Primary Work Transport: PT",
-                    id = "transport_pt")
-    }else if(input$demographic == "Primary Work Transport: Car")  {
-      tm_shape(ny_demographics) +
-        tm_polygons(col = "transport_car", 
-                    palette = vir, alpha = 0.6, border.alpha = 0.5, 
-                    title = "Primary Work Transport: Car",
-                    id = "transport_car")
-    }else {
-      tm_shape(ny_demographics) +
-        tm_polygons()
     
-    }
+    tm_shape(ny_demographics) +
+      tm_polygons()
     
   })
   
   output$covid <- renderPlot({
     g1 <- subset(COVID, date == input$date)
     ggplot(COVID, aes(x = date, y = cases_per_day)) + geom_point() + 
-      geom_point(data = g1, colour = "red", mapping = aes(size = 10),show.legend = FALSE )})
+      geom_point(data = g1, colour = "red", mapping = aes(size = 10),show.legend = FALSE ) +
+      #ggtitle() +
+      labs(title = "Daily New COVID-19 Infections in New York City, NY",
+           subtitle = "Red marker matches date on time slider",
+           x="Date", y= "COVID-19 New-Infections")
+  })
+  
+  
   
   output$cent1 <- renderTmap({
     edges_sf <- net2 %>% st_as_sf("edges")
     nodes_sf <- net2 %>% st_as_sf("nodes")
     
-    
-    
     tmap_mode("view")
     tm_shape(edges_sf) + 
       tm_lines(col = "gray50") +#"centrality") +
       tm_shape(nodes_sf) +
-      tm_dots(col = "cent_degree", id = "stop_name")
+      tm_dots(col = "cent_degree", id = "stop_name", , title = "Node Centrality - Degree")
   })
+  
+  
   output$cent2 <- renderTmap({
     edges_sf <- net2 %>% st_as_sf("edges")
     nodes_sf <- net2 %>% st_as_sf("nodes")
@@ -161,35 +115,141 @@ server <- function(input, output, session) {
     tm_shape(edges_sf) + 
       tm_lines(col = "gray50") +#"centrality") +
       tm_shape(nodes_sf) +
-      tm_dots(col = "cent_btwn", id = "stop_name")
+      tm_dots(col = "cent_btwn", id = "stop_name", title = "Node Centrality - Betweenness")
   })
   
   observe({
     ts_filter <- ts_combi_sf %>% filter(as.numeric(month) == as.numeric(format(input$date, "%m")))
     
     tmapProxy("map", session, {
-      #tm_remove_layer(401) +
-        tm_shape(ts_filter)+
-        tm_symbols(col = "remaining_ridership", 
-                   breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
-                   palette = inferno(4, direction = -1),
-                   size = 0.05, 
-                   alpha = 1, 
-                   title.col = "Remaining Ridership")
-    }) 
+      if(input$demographic == "Median Income") {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "income_median", 
+                      breaks = c(0, 50000, 100000, 150000, 250000),
+                      palette = vir, 
+                      alpha = 1, 
+                      border.alpha = 0.5, 
+                      title = "Median Household Income in 2019, USD",
+                      id = "income_median") +
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      } else if(input$demographic == "Race: Percent White")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "race_white", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Race: Pct White",
+                      id = "race_white")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else if(input$demographic == "Race: Percent Black")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "race_black", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Race: Pct Black",
+                      id = "race_black")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else if(input$demographic == "Race: Percent Asian")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "race_asian", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Race: Pct Asian",
+                      id = "race_asian")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else if(input$demographic == "Race: Percent Other")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "race_other", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Race: Pct Other",
+                      id = "race_other")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else if(input$demographic == "Race: Percent 2orMore")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "race_2ormore", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Race: Pct 2orMore",
+                      id = "race_2ormore")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else if(input$demographic == "Primary Work Transport: PT")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "transport_pt", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Primary Work Transport: PT",
+                      id = "transport_pt")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else if(input$demographic == "Primary Work Transport: Car")  {
+        tm_shape(ny_demographics) +
+          tm_polygons(col = "transport_car", 
+                      palette = vir, alpha = 1, border.alpha = 0.5, 
+                      title = "Primary Work Transport: Car",
+                      id = "transport_car")+
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+      }else {
+        tm_shape(ny_demographics) +
+          tm_polygons() +
+          tm_shape(ts_filter)+
+          tm_symbols(col = "remaining_ridership", 
+                     breaks = c(0, 10, 20, 30, 40,50,60,70,80,90, 100),
+                     palette = inferno(4, direction = -1),
+                     size = 0.05, 
+                     alpha = 1, 
+                     title.col = "Ridership 2020 vs. 2019 (pct)")
+        
+      }
+    })
+    
+
   })
   
-
-  
-  output$date_text <- renderPrint( {str(input$date)})
 }
 
 shinyApp(ui, server)
 
-#server <- function(input, output) {
-#  output$hist <- renderPlot({
-#    hist(rnorm(input$num))
-#  })
 
 
 
